36edd1c05076bcc9b4f2a6efcc6d811bb62431ed
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2021-08-13 14:45:25 -0500

    updated acquire

--- acquire.py
+++ acquire.py
@@ -1,11 +1,48 @@
-import pandas as pd 
+import pandas as pd
+import numpy as np
 import env
-from pydataset import data
+import os
 from env import host, user, password
 
-def get_connection(db, user=env.user, host=env.host, password=env.password):
+def get_connection(db, user=user, host=host, password=password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
+def new_titanic_data():
+    sql_query = 'SELECT * FROM passengers'
+    df = pd.read_sql(sql_query, get_connection('titanic_db'))
+    return df
+
+
 def get_titanic_data():
-    return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+    if os.path.isfile('titanic_df.csv'):
+        df = pd.read_csv('titanic_df.csv', index_col=0)
+    else:
+        df = new_titanic_data()
+        df.to_csv('titanic_df.csv')
+    return df
+
+def new_iris_data():
+    sql_query = """
+                SELECT
+                    species_id,
+                    species_name,
+                    measurement_id,
+                    sepal_length,
+                    sepal_width,
+                    petal_length,
+                    petal_width
+                FROM measurements
+                JOIN species USING(species_id)
+                """
+                
+    df = pd.read_sql(sql_query, get_connection('iris_db'))
+    return df
+
+def get_iris_data():
+    if os.path.isfile('iris_df.csv'):
+        df = pd.read_csv('iris_df.csv', index_col =0)
+    else:
+        df = new_iris_data()
+        df.to_csv('iris_df.csv')
+    return df
 
