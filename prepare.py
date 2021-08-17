49b3d304153735ded710a4f244f06abad248ac6c
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2021-08-17 16:59:32 -0500

    updated exercies

--- prepare.py
+++ prepare.py
@@ -1,6 +1,42 @@
-def clean_data(df):
+def clean_iris_data(df):
     df.drop_duplicates(inplace = True)
     df.drop(columns = ['species_id','measurement_id'], inplace = True)
     df.rename(columns = {"species_name": "species"}, inplace = True)
     dummy_df = pd.get_dummies(df[['species']], drop_first = True)
     return pd.concat([df, dummy_df], axis=1)
+
+def clean_data(df):
+    '''
+    This function will drop any duplicate observations, 
+    drop ['deck', 'embarked', 'class', 'age'], fill missing embark_town with 'Southampton'
+    and create dummy vars from sex and embark_town. 
+    '''
+    df = df.drop_duplicates()
+    df = df.drop(columns=['deck', 'embarked', 'class', 'age'])
+    df['embark_town'] = df.embark_town.fillna(value='Southampton')
+    dummy_df = pd.get_dummies(df[['sex', 'embark_town']], drop_first=True)
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+
+def split_data(df):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on survived.
+    return train, validate, test DataFrames.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.survived)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123, 
+                                       stratify=train_validate.survived)
+    return train, validate, test
+
+def prep_titanic_data(df):
+    '''
+    This function takes in a df and will drop any duplicate observations, 
+    drop ['deck', 'embarked', 'class', 'age'], fill missing embark_town with 'Southampton'
+    create dummy vars from sex and embark_town, and perform a train, validate, test split. 
+    Returns train, validate, and test DataFrames
+    '''
+    df = clean_data(df)
+    train, validate, test = split_data(df)
+    return train, validate, test
