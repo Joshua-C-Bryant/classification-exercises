1a71a242306da15b3bf3a69f3ecfd7518b7efebe
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2021-08-26 11:27:58 -0500

    updated prepare file

--- prepare.py
+++ prepare.py
@@ -1,44 +1,126 @@
 import pandas as pd
+import numpy as np
 
-def clean_iris_data(df):
-    df.drop_duplicates(inplace = True)
-    df.drop(columns = ['species_id','measurement_id'], inplace = True)
-    df.rename(columns = {"species_name": "species"}, inplace = True)
-    dummy_df = pd.get_dummies(df[['species']], drop_first = True)
-    return pd.concat([df, dummy_df], axis=1)
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
 
-def clean_data(df):
-    '''
-    This function will drop any duplicate observations, 
-    drop ['deck', 'embarked', 'class', 'age'], fill missing embark_town with 'Southampton'
-    and create dummy vars from sex and embark_town. 
-    '''
-    df = df.drop_duplicates()
-    df = df.drop(columns=['deck', 'embarked', 'class', 'age'])
-    df['embark_town'] = df.embark_town.fillna(value='Southampton')
-    dummy_df = pd.get_dummies(df[['sex', 'embark_town']], drop_first=True)
+###################### Prepare Iris Data ######################
+
+def prep_iris(df):
+
+    '''Prepares acquired Iris data for exploration'''
+    
+    # drop column using .drop(columns=column_name)
+    df = df.drop(columns='species_id')
+    
+    # remame column using .rename(columns={current_column_name : replacement_column_name})
+    df = df.rename(columns={'species_name':'species'})
+    
+    # create dummies dataframe using .get_dummies(column_name,not dropping any of the dummy columns)
+    dummy_df = pd.get_dummies(df['species'], drop_first=False)
+    
+    # join original df with dummies df using .concat([original_df,dummy_df], join along the index)
     df = pd.concat([df, dummy_df], axis=1)
+    
     return df
+
+###################### Prepare Iris Data With Split ######################
 
 def split_data(df):
     '''
-    take in a DataFrame and return train, validate, and test DataFrames; stratify on survived.
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on species.
     return train, validate, test DataFrames.
     '''
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.survived)
+    
+    # splits df into train_validate and test using train_test_split() stratifying on species to get an even mix of each species
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.species)
+    
+    # splits train_validate into train and validate using train_test_split() stratifying on species to get an even mix of each species
     train, validate = train_test_split(train_validate, 
                                        test_size=.3, 
                                        random_state=123, 
-                                       stratify=train_validate.survived)
+                                       stratify=train_validate.species)
     return train, validate, test
 
-def prep_titanic_data(df):
-    '''
-    This function takes in a df and will drop any duplicate observations, 
-    drop ['deck', 'embarked', 'class', 'age'], fill missing embark_town with 'Southampton'
-    create dummy vars from sex and embark_town, and perform a train, validate, test split. 
-    Returns train, validate, and test DataFrames
-    '''
-    df = clean_data(df)
+
+def prep_iris_with_split(df):
+    '''Prepares acquired Iris data for exploration'''
+    
+    # drop column using .drop(columns=column_name)
+    df = df.drop(columns='species_id')
+    
+    # remame column using .rename(columns={current_column_name : replacement_column_name})
+    df = df.rename(columns={'species_name':'species'})
+    
+    # create dummies dataframe using .get_dummies(column_name,not dropping any of the dummy columns)
+    dummy_df = pd.get_dummies(df['species'], drop_first=False)
+    
+    # join original df with dummies df using .concat([original_df,dummy_df], join along the index)
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    # split data into train/validate/test using split_data function
     train, validate, test = split_data(df)
+    
+    return train, validate, test
+
+def prep_titanic(df):
+    '''
+    This function take in the titanic data acquired by get_titanic_data,
+    Returns prepped train, validate, and test dfs with embarked dummy vars,
+    deck dropped, and the mean of age imputed for Null values.
+    '''
+    
+    # drop rows where embarked/embark town are null values
+    df = df[~df.embarked.isnull()]
+    
+    # encode embarked using dummy columns
+    titanic_dummies = pd.get_dummies(df.embarked, drop_first=True)
+    
+    # join dummy columns back to df
+    df = pd.concat([df, titanic_dummies], axis=1)
+    
+    # drop the deck column
+    df = df.drop(columns='deck')
+    
+    # split data into train, validate, test dfs
+    train, validate, test = titanic_split(df)
+    
+    # impute mean of age into null values in age column
+    train, validate, test = impute_mean_age(train, validate, test)
+    
+    return train, validate, test
+
+def titanic_split(df):
+    '''
+    This function take in the titanic data acquired by get_titanic_data,
+    performs a split and stratifies survived column.
+    Returns train, validate, and test dfs.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state=123, 
+                                        stratify=df.survived)
+    train, validate = train_test_split(train_validate, test_size=.3, 
+                                   random_state=123, 
+                                   stratify=train_validate.survived)
+    return train, validate, test
+
+
+def impute_mean_age(train, validate, test):
+    '''
+    This function imputes the mean of the age column for
+    observations with missing values.
+    Returns transformed train, validate, and test df.
+    '''
+    # create the imputer object with mean strategy
+    imputer = SimpleImputer(strategy = 'mean')
+    
+    # fit on and transform age column in train
+    train['age'] = imputer.fit_transform(train[['age']])
+    
+    # transform age column in validate
+    validate['age'] = imputer.transform(validate[['age']])
+    
+    # transform age column in test
+    test['age'] = imputer.transform(test[['age']])
+    
     return train, validate, test
